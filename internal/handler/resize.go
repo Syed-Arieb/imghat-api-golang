97bf34305de2295c28b8e42c95b29b5215612f41
Syed-Arieb <syedarieb56@gmail.com>
2026-05-16 15:28:38 +0500

    feat(handler): implement resize endpoint

--- internal/handler/resize.go
+++ internal/handler/resize.go
@@ -1,8 +1,36 @@
 package handler
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"imghat/internal/engine"
+
+	"strconv"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 // Resize will scale an image down to the requested dimensions.
 func Resize(c fiber.Ctx) error {
-	return stub(c, "resize")
+	data, err := readFile(c)
+	if err != nil {
+		return err
+	}
+
+	w, _ := strconv.Atoi(c.FormValue("width", "0"))
+	h, _ := strconv.Atoi(c.FormValue("height", "0"))
+
+	if w == 0 && h == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "at least one of width or height is required")
+	}
+
+	out, err := engine.Resize(data, engine.ResizeOptions{
+		Width:   w,
+		Height:  h,
+		Mode:    engine.ResizeMode(c.FormValue("mode", "fit")),
+		Quality: parseQuality(c.FormValue("quality", "80")),
+	})
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
+	}
+
+	return imageResponse(c, out)
 }
